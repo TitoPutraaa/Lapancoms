8d0de60b10e1df93827ec2b97305ce5bea5a9896
I Made Widiarta <madewidi185@gmail.com>
2025-11-01 09:31:17 +0800

    Melakukan perubahan di layout sidebar, index.css

--- Lapancoms/src/App.jsx
+++ Lapancoms/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Dashboard from "./components/layout/DashboardMain.jsx";
+import Sidebar from "./components/layout/Sidebar.jsx";
 
 import "./css/layout.css";
 
 function App() {
   return (
     <div className="">
+      {/* <Sidebar content={<Dashboard />} /> */}
       <Dashboard />
     </div>
   );
