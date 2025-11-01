892bd5dea5d5f72497fcf4c54f3226ef374e608c
I Made Widiarta <madewidi185@gmail.com>
2025-11-01 10:40:56 +0800

    buat login, buat route

--- Lapancoms/src/App.jsx
+++ Lapancoms/src/App.jsx
@@ -1,15 +1,25 @@
 import "./App.css";
 import Dashboard from "./components/layout/DashboardMain.jsx";
 import Sidebar from "./components/layout/Sidebar.jsx";
+import Login from "./pages/Login.jsx";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 import "./css/layout.css";
 
 function App() {
   return (
-    <div className="">
-      {/* <Sidebar content={<Dashboard />} /> */}
-      <Dashboard />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/admin" element={<Sidebar content={<Dashboard />} />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
