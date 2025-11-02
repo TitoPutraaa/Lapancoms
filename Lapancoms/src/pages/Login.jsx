e5b20b0bf4753e99380125c5cf2a471c28609a22
I Made Widiarta <madewidi185@gmail.com>
2025-11-02 09:50:10 +0800

    add plugin prettier, fix style css in indexcss, login, and sidebar

--- Lapancoms/src/pages/Login.jsx
+++ Lapancoms/src/pages/Login.jsx
@@ -14,24 +14,24 @@ export default function Login() {
 
   return (
     <>
-      <div className="flex w-full h-dvh items-center justify-center">
-        <div className="inset-0 fixed z-0 bg-black">
+      <div className="flex h-screen w-full items-center justify-center">
+        <div className="fixed inset-0 z-0 bg-black">
           <img
             src={assets.loginIMG}
             alt=""
-            className="w-full h-full object-cover blur-3xl opacity-63"
+            className="h-full w-full object-cover opacity-63 blur-3xl"
           />
         </div>
-        <div className="bg-white w-fit flex h-fit relative rounded-2xl overflow-hidden">
+        <div className="relative flex h-fit w-fit overflow-hidden rounded-2xl bg-white">
           <div className="max-w-lg px-16 py-20">
-            <h1 className="font-semibold text-3xl text-dark mb-8">
+            <h1 className="text-dark mb-8 text-3xl font-semibold">
               Nice to see you again
             </h1>
             <form>
               <div className="mb-5">
                 <label
                   htmlFor="username"
-                  className="block mb-2 text-sm text-dark"
+                  className="text-dark mb-2 block text-sm"
                 >
                   Username
                 </label>
@@ -39,7 +39,7 @@ export default function Login() {
                   type="text"
                   name="username"
                   id="username"
-                  className="bg-slate-100 border border-slate-300 text-dark text-sm rounded-lg focus:ring-slate-500 focus:border-slate-500 block w-full p-2.5 "
+                  className="text-dark block w-full rounded-lg border border-slate-300 bg-slate-100 p-2.5 text-sm focus:border-slate-500 focus:ring-slate-500"
                   placeholder="Enter username"
                   required
                 />
@@ -47,21 +47,21 @@ export default function Login() {
               <div className="mb-8">
                 <label
                   htmlFor="password"
-                  className="block mb-2 text-sm text-dark"
+                  className="text-dark mb-2 block text-sm"
                 >
                   Password
                 </label>
-                <div className="relative ">
+                <div className="relative">
                   <input
                     type={invisible ? "password" : "text"}
                     name="password"
                     id="password"
-                    className="bg-slate-100 border border-slate-300 text-dark text-sm rounded-lg focus:ring-slate-500 focus:border-slate-500 block w-full p-2.5 pr-10"
+                    className="text-dark block w-full rounded-lg border border-slate-300 bg-slate-100 p-2.5 pr-10 text-sm focus:border-slate-500 focus:ring-slate-500"
                     placeholder="Enter password"
                     required
                   />
                   <span
-                    className="absolute top-1/2 -translate-y-1/2 right-4 cursor-pointer select-none"
+                    className="absolute top-1/2 right-4 -translate-y-1/2 cursor-pointer select-none"
                     onClick={toggleInvisible}
                   >
                     {invisible ? "👁" : "😫"}
@@ -70,18 +70,18 @@ export default function Login() {
               </div>
               <button
                 type="submit"
-                className="text-white bg-sky-700 hover:bg-sky-800 focus:ring-2 focus:outline-none focus:ring-sky-600 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center "
+                className="bg-primary hover:bg-primary/95 w-full rounded-lg px-5 py-2.5 text-center text-sm font-medium text-white transition-colors focus:ring-2 focus:ring-sky-600 focus:outline-none"
                 onClick={() => navigate("/admin")}
               >
                 Login
               </button>
             </form>
           </div>
-          <div className="hidden lg:block max-w-md">
+          <div className="hidden max-w-md lg:block">
             <img
               src={assets.loginIMG}
               alt=""
-              className="object-cover w-full h-full bg-dark/80"
+              className="bg-dark/80 h-full w-full object-cover"
             />
           </div>
         </div>
