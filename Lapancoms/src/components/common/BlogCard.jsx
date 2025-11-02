a1155717850418dd993e7408f9a1608218e29f19
Tito Putra Bamulo <titobamulo13@gmail.com>
2025-11-02 14:09:13 +0800

    add icon in date

--- Lapancoms/src/components/common/BlogCard.jsx
+++ Lapancoms/src/components/common/BlogCard.jsx
@@ -8,8 +8,13 @@ export default function BlogCard({ title, date, author }) {
           <img className="rounded-3xl" src={assets.blogCardImg} alt="" />
 
           <div className="">
-            <div className="px-1.5 py-1 bg-white font-normal absolute bottom-2.5 left-2.5  rounded-lg ">
+            <div className="flex px-1.5 py-1 bg-white font-normal absolute bottom-2.5 left-2.5  rounded-lg ">
+              <svg class="w-4 h-4 text-black my-auto " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                  <path d="M0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm14-7.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm0 4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm-5-4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm0 4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm-5-4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm0 4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1ZM20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4Z"/>
+              </svg>
+              <div className="ml-2 pl-2 border-l-2 border-gray-500">
               {date}
+              </div>
             </div>
           </div>
         </div>
