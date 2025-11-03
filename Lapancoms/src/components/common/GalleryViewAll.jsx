45fad94d6672f6c47ce5bff52a4055f24bc196b1
Tito Putra Bamulo <titobamulo13@gmail.com>
2025-11-03 16:20:46 +0800

    update node module and fix error

--- Lapancoms/src/components/common/GalleryViewAll.jsx
+++ Lapancoms/src/components/common/GalleryViewAll.jsx
@@ -1,13 +1,29 @@
-export default function BlogViewAll() { 
+export default function BlogViewAll() {
   return (
-          <a href="/allGallery" className="flex mr-5 border-2 rounded-3xl border-primary hover:bg-primary hover:text-white transition duration-1000 ease-in-out">
-            <div className="text-white bg-primary px-3 py-1 rounded-3xl">View All</div>
-            <div className="mr-2 mt-1.5">
-              <svg className="" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round">
-                <line x1="7" y1="17" x2="17" y2="7"></line>
-                <polyline points="7 7 17 7 17 17"></polyline>
-              </svg>
-            </div>
-          </a>    
-  )
+    <a
+      href="/allGallery"
+      className="border-primary hover:bg-primary mr-5 flex rounded-3xl border-2 transition duration-1000 ease-in-out hover:text-white"
+    >
+      <div className="bg-primary rounded-3xl px-3 py-1 text-white">
+        View All
+      </div>
+      <div className="mt-1.5 mr-2">
+        <svg
+          className=""
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="3"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <line x1="7" y1="17" x2="17" y2="7"></line>
+          <polyline points="7 7 17 7 17 17"></polyline>
+        </svg>
+      </div>
+    </a>
+  );
 }
