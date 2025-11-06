91cadb231feb1ebb6f4eaef7297c0e926799183e
Tito Putra Bamulo <titobamulo13@gmail.com>
2025-11-07 04:31:28 +0800

    fix sidebar bug post

--- Lapancoms/src/components/common/BackBtn.jsx
+++ Lapancoms/src/components/common/BackBtn.jsx
@@ -1,14 +1,22 @@
 export default function BackBtn() {
-    return (
-            <div
-              className="inline-flex items-center text-white text-sm text-center py-1 px-5 rounded-4xl bg-primary  transition duration-700 ease-in-out"
-            >
-                <svg class="w-4 h-4 mr-2 text-white " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5H1m0 0 4 4M1 5l4-4"/>
-                </svg>          
-              Back
-
-
-            </div>
-    )
+  return (
+    <div className="bg-primary inline-flex items-center rounded-4xl px-5 py-1 text-center text-sm text-white transition duration-700 ease-in-out">
+      <svg
+        class="mr-2 h-4 w-4 text-white"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 14 10"
+      >
+        <path
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M13 5H1m0 0 4 4M1 5l4-4"
+        />
+      </svg>
+      Back
+    </div>
+  );
 }
--- Lapancoms/src/components/common/BlogViewAll.jsx
+++ Lapancoms/src/components/common/BlogViewAll.jsx
@@ -16,9 +16,9 @@ export default function BlogViewAll() {
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
-              stroke-width="3"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="3"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             >
               <line x1="7" y1="17" x2="17" y2="7"></line>
               <polyline points="7 7 17 7 17 17"></polyline>
