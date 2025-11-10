7f532c2cd06772c204bdf549d76f729bf5ff79da
aaa13riey <aritrisnadewi99@gmail.com>
2025-11-10 13:51:03 +0800

    update comfirm button

--- Lapancoms/src/components/common/ComfirmBtn.jsx
+++ Lapancoms/src/components/common/ComfirmBtn.jsx
@@ -1,7 +1,8 @@
 export default function ComfirmBtn() {
     return(
         <div>
-            <button type="button" class="text-white bg-primary hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Comfirm</button>
-        </div>
+  <button type="button" className="text-white bg-primary hover:opacity-90 w-100 font-poppins rounded-lg text-lg py-3">Comfirm</button>
+</div>
+
     );
 }
