dca4136e0077e574ef4ac7a391955b9ecc1c6c1b
Tito Putra Bamulo <titobamulo13@gmail.com>
2025-11-20 23:04:40 +0800

    add validate delate admin

--- Lapancoms/src/components/common/DeletedAdmin.jsx
+++ Lapancoms/src/components/common/DeletedAdmin.jsx
@@ -1,60 +1,48 @@
-import { useState } from "react";
 import { BsTrash } from "react-icons/bs";
-import { IoClose } from "react-icons/io5";
 
-export default function DeletedAdmin() {
-  // const [onClose, setOnClose] = useState(false);
-  // const [onDelate, setonDelate] = useState(false);
-  // if (isOpen) return null;
-  const [isOpen, setIsOpen] = useState(true);
-
-  const handleClose = () => {
-    if (isOpen) {
-      setIsOpen(false);
-    }
-  };
+export default function DeletedAdmin({ view, setView, handleDelete, idDel }) {
+  if (!view) return null;
 
   return (
     <>
-      {isOpen && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
-          <div className="w-full max-w-lg rounded-xl bg-white p-6 shadow-xl">
-            <div className="flex">
-              <div className="flex h-12 w-12 items-center justify-center rounded-full bg-red-100">
-                <BsTrash className="h-6 w-6 text-red-600" />
-              </div>
-
-              <div className="ml-4">
-                <h3 className="text-lg font-semibold text-gray-900">
-                  Delete Admin
-                </h3>
-                <p className="mt-1 text-sm text-gray-500">
-                  Are you sure you want to remove this admin? You won't be able
-                  to undo this.
-                </p>
-              </div>
+      <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
+        <div className="w-full max-w-lg rounded-xl bg-white p-6 shadow-xl">
+          <div className="flex">
+            <div className="flex h-12 w-12 items-center justify-center rounded-full bg-red-100">
+              <BsTrash className="h-6 w-6 text-red-600" />
             </div>
 
-            <div className="mt-8 flex justify-end gap-3">
-              <button
-                onClick={handleClose}
-                type="button"
-                className="rounded-lg border border-gray-300 bg-white px-4 py-2 text-sm font-semibold text-gray-700 hover:bg-gray-100"
-              >
-                Cancel
-              </button>
-
-              <button
-                onClick={handleClose}
-                type="button"
-                className="rounded-lg bg-red-600 px-4 py-2 text-sm font-semibold text-white hover:bg-red-700"
-              >
-                Delete
-              </button>
+            <div className="ml-4">
+              <h3 className="text-lg font-semibold text-gray-900">
+                Delete Admin
+              </h3>
+              <p className="mt-1 text-sm text-gray-500">
+                Are you sure you want to remove{" "}
+                <span className="text-dark font-bold">{idDel.username}</span> ?
+                You won't be able to undo this.
+              </p>
             </div>
           </div>
+
+          <div className="mt-8 flex justify-end gap-3">
+            <button
+              onClick={() => setView(false)}
+              type="button"
+              className="rounded-lg border border-gray-300 bg-white px-4 py-2 text-sm font-semibold text-gray-700 hover:bg-gray-100"
+            >
+              Cancel
+            </button>
+
+            <button
+              onClick={() => handleDelete(idDel.idAdmin)}
+              type="button"
+              className="rounded-lg bg-red-600 px-4 py-2 text-sm font-semibold text-white hover:bg-red-700"
+            >
+              Delete
+            </button>
+          </div>
         </div>
-      )}
+      </div>
     </>
   );
 }
