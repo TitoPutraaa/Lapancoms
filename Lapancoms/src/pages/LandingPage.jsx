4e03ba39de548acd327b198acaac9669769d5f8f
I Made Widiarta <madewidi185@gmail.com>
2025-12-20 15:13:13 +0800

    blog lp v2

--- Lapancoms/src/pages/LandingPage.jsx
+++ Lapancoms/src/pages/LandingPage.jsx
@@ -57,9 +57,13 @@ export default function LandingPage() {
               </h2>
             </div>
             <div className="">
-              {/* <button className="rounded-full bg-emerald-600 font-medium text-white transition hover:bg-emerald-700 md:px-8 md:py-3">
-                View All
-              </button> */}
+              <div className="group relative w-26 cursor-pointer overflow-hidden rounded-xl bg-white transition duration-500 md:w-29">
+                <BsArrowRight className="text-accent absolute top-1/2 left-0 -translate-y-1/2 stroke-1 opacity-0 transition duration-500 group-hover:translate-x-2 group-hover:opacity-100" />
+                <div className="group-hover:bg-accent relative z-1 w-fit rounded-xl bg-emerald-600 px-2.5 py-1.5 text-sm text-white transition-all duration-500 group-hover:translate-x-7 md:px-3 md:text-base">
+                  View All
+                </div>
+                <BsArrowUpRight className="absolute top-1/2 right-2 ml-2 h-4 w-4 -translate-y-1/2 stroke-1 text-emerald-600 transition duration-500 group-hover:translate-x-4 group-hover:-translate-y-4 group-hover:opacity-0" />
+              </div>
             </div>
           </div>
           <div className="relative overflow-hidden">
